73aa98cc163d60b6a5930c6fc2de2efa3a83184e
Ilya <iljuha.alikin@gmail.com>
2024-03-31 13:13:20 +0500

    added swiper

--- src/js/app.js
+++ src/js/app.js
@@ -6,25 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let tabButtons = document.querySelectorAll(".tablink");
   let tradeTabButtons = document.querySelectorAll(".trade-tools-tablink");
 
-  tabButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-      let tabContents = document.querySelectorAll(".tabcontent");
-      tabContents.forEach(function (content) {
-        content.style.display = "none";
-        content.classList.remove("active");
-      });
+  // tabButtons.forEach(function (button) {
+  //   button.addEventListener("click", function () {
+  //     let tabContents = document.querySelectorAll(".tabcontent");
+  //     tabContents.forEach(function (content) {
+  //       content.style.display = "none";
+  //       content.classList.remove("active");
+  //     });
 
-      tabButtons.forEach(function (btn) {
-        btn.classList.remove("active");
-      });
+  //     tabButtons.forEach(function (btn) {
+  //       btn.classList.remove("active");
+  //     });
 
-      let tabName = this.id.replace("tab", "section");
-      document.getElementById(tabName).style.display = "block";
-      document.getElementById(tabName).classList.add("active");
+  //     let tabName = this.id.replace("tab", "section");
+  //     document.getElementById(tabName).style.display = "block";
+  //     document.getElementById(tabName).classList.add("active");
 
-      this.classList.add("active");
-    });
-  });
+  //     this.classList.add("active");
+  //   });
+  // });
 
   tradeTabButtons.forEach(function (button) {
     button.addEventListener("click", function () {
@@ -55,4 +55,65 @@ document.addEventListener("DOMContentLoaded", function () {
     nav.classList.toggle("active");
     body.classList.toggle("lock");
   });
+
+  const swiper = new Swiper(".swiper", {
+    loop: false,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: true,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+
+  const swiperButtons = document.querySelectorAll(".tablink");
+  const availablePlatformsWrapper = document.querySelector(
+    ".available-platforms__wrapper"
+  );
+
+  function updateActiveTabButtonColor(activeIndex) {
+    swiperButtons.forEach((button, index) => {
+      if (index === activeIndex) {
+        if (activeIndex === 0) {
+          button.style.color = "#ea5504";
+        } else if (activeIndex === 1) {
+          button.style.color = "#fabe00";
+        } else if (activeIndex === 2) {
+          button.style.color = "#74c6be";
+        }
+      } else {
+        button.style.color = "black";
+      }
+    });
+  }
+
+  function updateActiveWrapperColor(activeIndex) {
+    if (activeIndex === 0) {
+      availablePlatformsWrapper.style.borderColor = "#ea5504"; // Change border color
+    } else if (activeIndex === 1) {
+      availablePlatformsWrapper.style.borderColor = "#fabe00"; // Change border color
+    } else if (activeIndex === 2) {
+      availablePlatformsWrapper.style.borderColor = "#74c6be"; // Change border color
+    }
+  }
+
+  swiperButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      swiper.slideTo(index);
+    });
+  });
+
+  swiper.on("slideChange", () => {
+    const activeIndex = swiper.realIndex;
+    updateActiveTabButtonColor(activeIndex);
+    updateActiveWrapperColor(activeIndex);
+  });
+
+  updateActiveTabButtonColor(swiper.realIndex);
+  updateActiveWrapperColor(swiper.realIndex);
 });
