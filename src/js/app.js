951bf5ce1c3db03ba47d38373749c06ed88ab73c
Ilya <iljuha.alikin@gmail.com>
2024-04-04 17:47:03 +0500

    added preloader

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,15 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+document.body.onload = function () {
+  setTimeout(function () {
+    const preloader = document.getElementById("page-preloader");
+    if (!preloader.classList.contains("done")) {
+      preloader.classList.add("done");
+    }
+  }, 1500);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   let burger = document.getElementById("burger");
 
